92f8a1be6c09ffdf289bb188a18395ac0287991e
Faisal Abdul Slam <36113693+Faisal0385@users.noreply.github.com>
2021-02-20 23:22:41 +0600

    Email Slicer with Python
    Email Slicer with Python

---
+++ cheat_sheet/practice_list.py
@@ -0,0 +1,42 @@
+## Formula : list_name = [element1,element2]
+
+## How to create a list
+
+## Create an empty list
+list_name = "Faisal"
+print(list_name)
+
+## Create a list with some element
+list_nums = [1,2.2,"Faisal", True]
+print(list_nums)
+
+## Create a list with split() funtion
+text = "I love python !!"
+list_split = text.split(" ")
+print(list_split)
+
+
+## How to access a list
+## list_names = ["Faisal", "Faezah", "Fahad", "Fahim", "Montaha", "Farjana", "Rajon"]
+    ## By index access the 1st, 4th & last element of the given list
+    ## By slice operator
+
+## List mutable
+    ## create a list with variable "list_name" with 5 names
+    ## modify the mid, last elements with new element
+
+## Traversing elements of a list
+    ## using while loop
+    ## using for loop
+
+## Information about list
+    ## a) length of a list
+    ## b) check duplication
+    ## c) check index
+
+## Manipulating elements of a list
+    ## a) add an element at the end of the list
+    ## b) add an element at a specified index position
+
+## Merge two lists together
+## remove specified item from a list
